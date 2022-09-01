f33b18a55d34639211c830b892e5b1b8cfcdc812
anshuxinha <anshuxinha@gmail.com>
2022-09-01 12:54:30 +0530

    hex to hsl script

--- src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './convert';
-export * from './invert';
-export { ILLUMINANT_D65, ILLUMINANT_C, SRGB, ADOBE_RGB } from './colorspace';
--- test/index.test.ts
+++
@@ -1,8 +0,0 @@
-import { ILLUMINANT_C, munsellToRgb255 } from '../src/index';
-
-describe('main.js', () => {
-  test('exported', () => {
-    expect(ILLUMINANT_C.catMatrixThisToC).toBeDefined();
-    expect(munsellToRgb255).toBeInstanceOf(Function);
-  });
-});
